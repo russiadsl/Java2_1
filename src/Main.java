9dc151bf1c61625eb694814df9e976e745a3fed8
Evgeny Bezugly <bezugly_spb@mail.ru>
2021-11-28 03:01:38 +0300

    Add bonus system

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Объявляете переменные для входных данных и
+        int ticketCost = 250;
+        int bonus = 5;
+        int bonusSum = ticketCost * bonus / 100;
+
+        System.out.println(bonusSum);
+    }
+}
